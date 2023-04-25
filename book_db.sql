4e47ad9bced3a070357b1c57bb12f39316a71dd7
Elit3-Looser27 <bd2101453@tkh.edu.eg>
2023-04-25 23:40:31 +0200

    Updated sql file

--- book_db.sql
+++ book_db.sql
@@ -1,32 +1,110 @@
 -- phpMyAdmin SQL Dump
-
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 25, 2023 at 11:36 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
--- Database name: `book_db`
--- Tables structure for table `book_form`
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `book_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_form`
+--
 
 CREATE TABLE `book_form` (
-  `id` int(255) PRIMARY NOT NULL,
+  `id` int(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `email` varchar(255) UNIQUE NOT NULL,
+  `email` varchar(255) NOT NULL,
   `phone` int(15) NOT NULL,
   `address` varchar(255) NOT NULL,
   `location` varchar(255) NOT NULL,
   `guests` int(255) NOT NULL,
   `arrivals` date NOT NULL,
   `leaving` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `book_form`
+--
+
+INSERT INTO `book_form` (`id`, `name`, `email`, `phone`, `address`, `location`, `guests`, `arrivals`, `leaving`) VALUES
+(7, 'Dania', 'Dania@gmail.com', 1234567, 'Maadi, cairo', 'Paris', 4, '2023-04-26', '2023-05-10'),
+(8, 'Ali gabr', 'ali@gmail.com', 123456789, 'Maadi, cairo', 'Canada', 2, '2023-05-16', '2023-08-22');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
 
 CREATE TABLE `user` (
-  `id` int(255) PRIMARY NOT NULL,
+  `id` int(255) NOT NULL,
   `name` varchar(128) NOT NULL,
-  `email` varchar(255) UNIQUE NOT NULL,
-  `password_hash`varchar(255)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `email` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `password_hash`) VALUES
+(7, 'Begad Hatem', 'begad@gmail.com', '$2y$10$HuBeTZVBQav4iqzKHgpzfuKv4sEd6ZGa3hxw3adL2GIxCIjNgaAzC'),
+(14, 'Enas', 'Enas@gmail.com', '$2y$10$DsEHV31zKC.iJZG3ZAdsxOT6AzJ3IQrFfLYSWPUWhyK.AcpckK1l2'),
+(15, 'ali gabr', 'ali@gmail.com', '$2y$10$J9VRL4XS2NyEivSLpdjhLuaD1EQ9emAR4dJNHmCW5psmUKFTk7Ri2'),
+(16, 'Abdelrahaman Roshdi', 'boddi@gmail.com', '$2y$10$hdLVKFdWkhPZXEuLzRbW..Ne7lItVov.cGvDKcZGirkn..TC9GnwG');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_form`
+--
+ALTER TABLE `book_form`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_form`
+--
+ALTER TABLE `book_form`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
